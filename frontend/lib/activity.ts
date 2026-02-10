0cdf8fc80b1e68e322258dafb4714607a48e0e35
Jeysi17 <johncarlculanding@gmail.com>
2026-02-10 16:44:19 +0800

    DAY 2.10

--- frontend/lib/activity.ts
+++
@@ -1,3 +0,0 @@
-// Re-export from TSX module (this file exists so resolvers that prefer .ts still work)
-export type { ActivityItem } from './activity.tsx'
-export { formatActionLabel, getActionIcon, timeAgo } from './activity.tsx'
